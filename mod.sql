dc63d2786873dcbc6eb829a6f26d2d963fd754c7
XGG <zapperhatz26@gmail.com>
2018-03-07 21:52:11 +0800

    corrected table name

--- mod.sql
+++ mod.sql
@@ -157,4 +157,4 @@ UPDATE `m_lib_barangay` SET `brgy_id` = `barangay_id`;
 UPDATE m_lib_barangay SET brgy_id=concat(0,brgy_id);
 
 update game_user set user_password = old_password('');
-INSERT into menu_by_location values ('CONS','phie',1382);
+INSERT into module_menu_location values ('ADM','phie',1382);
